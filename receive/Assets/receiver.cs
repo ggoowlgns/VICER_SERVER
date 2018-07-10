d5a87ee1cdf5b2abbba38e61d31769a9e74514b2
rihjeo <39438575+rihjeo@users.noreply.github.com>
2018-07-10 15:47:08 +0900

    recv

--- receive/Assets/receiver.cs
+++ receive/Assets/receiver.cs
@@ -5,15 +5,20 @@ using System.Net.Sockets;
 using System.Net;
 using System.IO;
 using System;
+using System.Text;
 
 public class receiver : MonoBehaviour
 {
+    //Socket sck;
     public RawImage image;
     public bool enableLog = false;
+    
 
     const int port = 8010;
-    public string IP = "192.168.35.211";
+    string IP = "192.168.35.211";
     TcpClient client;
+    NetworkStream NS = null;
+    StreamWriter SW = null;
 
     Texture2D tex;
 
@@ -28,18 +33,39 @@ public class receiver : MonoBehaviour
         Application.runInBackground = true;
 
         tex = new Texture2D(0, 0);
+        //sck = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        //IPEndPoint localEndPoint = new IPEndPoint(IPAddress.Parse(IP), port);
+
         client = new TcpClient();
+        
 
         //Connect to server from another Thread
         Loom.RunAsync(() =>
         {
             Debug.Log("Connecting to server...");
             // if on desktop
-            client.Connect(IPAddress.Parse(IP), port);
+            //client.Connect(IPAddress.Parse(IP), port);
 
             // if using the IPAD
-            //client.Connect(IPAddress.Parse(IP), port);
+            client.Connect(IPAddress.Parse(IP), port);
+            NS = client.GetStream();
+            SW = new StreamWriter(NS, Encoding.UTF8);
+            Debug.Log("Connection");
+            string SendMessage = null;
+            SendMessage = " ";
+            Debug.Log("er");
+            SW.Write(SendMessage); // 메시지 보내기
+            Debug.Log("er2");
+            SW.Flush();
+            Debug.Log("er3");
+            //Debug.Log("Connection");
+
+
+            /*sck.Connect(localEndPoint);
             Debug.Log("Connected!");
+            string text = "get";
+            byte[] data = Encoding.UTF8.GetBytes(text);
+            sck.Send(data);*/
 
             imageReceiver();
         });
@@ -54,11 +80,29 @@ public class receiver : MonoBehaviour
             while (!stop)
             {
                 //Read Image Count
-                int imageSize = readImageByteSize(SEND_RECEIVE_COUNT);
-                Debug.Log("Received Image byte Length: " + imageSize);
+                 int imageSize = readImageByteSize(SEND_RECEIVE_COUNT);
+                 LOGWARNING("Received Image byte Length: " + imageSize);
 
-                //Read Image Bytes and Display it
-                readFrameByteArray(imageSize);
+                 //Read Image Bytes and Display it
+                 readFrameByteArray(imageSize);
+
+                /*Byte[] _data = new byte[15];
+                Debug.Log("Accept");
+                sck.Receive(_data);
+
+                int length = BitConverter.ToInt32(_data, 0);
+                Byte[] rdata = new byte[length];
+                sck.Receive(rdata);
+                bool readyToReadAgain = false;
+                Loom.QueueOnMainThread(() =>
+                {
+                    displayReceivedImage(rdata);
+                    readyToReadAgain = true;
+                });
+                while (!readyToReadAgain)
+                {
+                    System.Threading.Thread.Sleep(1);
+                }*/
             }
         });
     }
@@ -94,7 +138,8 @@ public class receiver : MonoBehaviour
         do
         {
             var read = serverStream.Read(imageBytesCount, total, size - total);
-            //Debug.LogFormat("Client recieved {0} bytes", total);
+            Debug.LogFormat("Client recieved {0} bytes", total);
+            Debug.LogFormat("size {0}", size);
             if (read == 0)
             {
                 disconnected = true;
@@ -127,7 +172,8 @@ public class receiver : MonoBehaviour
         do
         {
             var read = serverStream.Read(imageBytes, total, size - total);
-            //Debug.LogFormat("Client recieved {0} bytes", total);
+            Debug.LogFormat("Client recieved {0} bytes", total);
+            Debug.LogFormat("size {0}", size);
             if (read == 0)
             {
                 disconnected = true;
@@ -167,7 +213,6 @@ public class receiver : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
 
     }
 
